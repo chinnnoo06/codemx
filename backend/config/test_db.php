d2100e649ea8b6e4e73a8b2b0551969e80aacb02
chinnoo_06 <inda5054@gmail.com>
2025-01-07 16:18:38 -0600

    actualizar

--- backend/config/test_db.php
+++ backend/config/test_db.php
@@ -1,22 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Datos de conexión
-$servername = "localhost"; // O el servidor que te indica Hostinguer
-$username = "u499414612_codemx_user"; // Usuario de tu base de datos
-$password = "Mandarino254"; // Contraseña de tu base de datos
-$dbname = "u499414612_codemx"; // Nombre de tu base de datos
-
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
 
 echo "Conexión exitosa a la base de datos";
-$conn->close();
 ?>
