2ed169d1d071d1b64f34bd462e28456df3b0a64b
chinnoo_06 <inda5054@gmail.com>
2025-01-07 12:46:11 -0600

    actualizar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,12 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import { RutasBienvenida } from './routers/RutasBienvenida';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { PageLogin } from './pages/login-crearcuenta-recuperar/PageLogin';
-import { PageRecuperar } from './pages/login-crearcuenta-recuperar/PageRecuperar';
-import { PageCrearCuenta } from './pages/login-crearcuenta-recuperar/PageCrearCuenta';
+import React from 'react';
 
 function App() {
-   return (
-    <BrowserRouter basename="/codemx/frontend/build">
-        <Routes>
-            <Route path="/" element={<RutasBienvenida />} />
-            <Route path="/bienvenida" element={<RutasBienvenida />} />
-            <Route path="/crear-cuenta" element={<PageCrearCuenta />} />
-            <Route path="/iniciar-sesion" element={<PageLogin />} />
-            <Route path="/recuperar-password" element={<PageRecuperar />} />
-            <Route path="/recuperar-password/:token" element={<PageRecuperar />} />
-        </Routes>
-    </BrowserRouter>
-
-   )
+  return (
+    <div>
+      <h1>¡La aplicación React está funcionando correctamente!</h1>
+      <p>Si ves este mensaje, React se está cargando correctamente en el servidor.</p>
+    </div>
+  );
 }
 
 export default App;
- 
