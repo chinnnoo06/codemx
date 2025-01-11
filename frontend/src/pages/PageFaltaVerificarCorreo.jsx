a22d4893dadbd16ed964e95eba3c81270c2de565
chinnoo_06 <inda5054@gmail.com>
2025-01-10 22:14:24 -0600

    act

--- frontend/src/pages/PageFaltaVerificarCorreo.jsx
+++ frontend/src/pages/PageFaltaVerificarCorreo.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import logo from '../resources/logo.png';
+import '../styles/login-crearcuenta-recuperar/form.css';
 
 export const PageFaltaVerificarCorreo = () => {
 
   return (
     <div>
 
-      {/* Header */}
-      <div className="contenedor-header container-fluid w-100">
-        <header className="d-flex justify-content-center align-items-center">
-          <div className="logo">
-            <Link to="/"><img src={logo} alt="Logo" /></Link>
-          </div>
-        </header>
-      </div>
+        {/* Header */}
+        <div className="contenedor-header container-fluid w-100">
+            <header className="d-flex justify-content-center align-items-center">
+            <div className="logo">
+                <Link to="/"><img src={logo} alt="Logo" /></Link>
+            </div>
+            </header>
+        </div>
 
-      <div className="verificacion-correo text-center py-5">
-        <h2 className="mb-3">¡Ups! Falta verificar tu cuenta</h2>
-        <p className="mb-4">
-            Hemos enviado un enlace de verificación a tu correo electrónico
-        </p>
-        <p>
-            Por favor, revisa tu bandeja de entrada y sigue las instrucciones para
-            verificar tu cuenta.
-        </p>
-
-    </div>
+        <div className="verificacion-correo text-center py-5">
+            <h2 className="texto-color mb-3">¡Ups! Falta verificar tu cuenta</h2>
+            <p className="texto-color mb-4">
+                Hemos enviado un enlace de verificación a tu correo electrónico
+            </p>
+            <p className="texto-color">
+                Por favor, revisa tu bandeja de entrada y sigue las instrucciones para
+                verificar tu cuenta.
+            </p>
+        </div>
 
 
     </div>
--- frontend/src/pages/PageFaltaVerificarRFC.jsx
+++ frontend/src/pages/PageFaltaVerificarRFC.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import logo from '../resources/logo.png';
+import '../styles/login-crearcuenta-recuperar/form.css';
 
 export const PageFaltaVerificarRFC = () => {
   return (
-    <div>PageFaltaVerificarRFC</div>
+    <div>
+
+        {/* Header */}
+        <div className="contenedor-header container-fluid w-100">
+            <header className="d-flex justify-content-center align-items-center">
+            <div className="logo">
+                <Link to="/"><img src={logo} alt="Logo" /></Link>
+            </div>
+            </header>
+        </div>
+
+        <div className="verificacion-correo text-center py-5">
+            <h2 className="texto-color mb-3">¡Espera! Tu cuenta de empresa esta en proceso de verificación</h2>
+            <p className="texto-color mb-4">
+                En este momento nuestro equipo esta verificando la validez del RFC proporcionado durante el proceso de registro.
+            </p>
+            <p className="texto-color">
+                Espera porfavor, cuando termine el proceso te llegará un correo electronico informandote, gracias por tu comprencion :)
+            </p>
+        </div>
+
+
+</div>
   )
 }
