8fb27d0fa66613e27f6f6f12c3095caee0b080ec
chinnoo_06 <inda5054@gmail.com>
2025-01-13 15:40:31 -0600

    Act

--- backend/candidato/obtener_datos_candidato.php
+++ backend/candidato/obtener_datos_candidato.php
@@ -4,28 +4,28 @@ require_once '../config/conexion.php';
 try {
     // Consulta para obtener los datos completos del candidato y los nombres de las tablas relacionadas
     $consulta = "
-        SELECT 
-            candidato.ID,
-            candidato.Nombre,
-            candidato.Apellido,
-            candidato.Fecha_Nacimiento,
-            candidato.Email,
-            candidato.Telefono,
-            candidato.Direccion,
-            estado.Nombre,
-            sexo.Sexo,
-            candidato.Fotografia,
-            universidad.Nombre,
-            candidato.Tiempo_Restante,
-            modalidad_trabajo.Modalidad,
-            candidato.CV
-        FROM candidato
-        INNER JOIN estado ON candidato.Estado = estado.ID
-        INNER JOIN sexo ON candidato.Sexo = sexo.ID
-        INNER JOIN universidad ON candidato.Universidad = universidad.ID
-        INNER JOIN modalidad_trabajo ON candidato.Modalidad_Trabajo = modalidad_trabajo.ID
-        WHERE candidato.ID = 1
-        LIMIT 1
+    SELECT 
+        candidato.ID,
+        candidato.Nombre AS Candidato_Nombre,
+        candidato.Apellido,
+        candidato.Fecha_Nacimiento,
+        candidato.Email,
+        candidato.Telefono,
+        candidato.Direccion,
+        estado.Nombre AS Estado_Nombre,
+        sexo.Sexo AS Sexo_Nombre,
+        candidato.Fotografia,
+        universidad.Nombre AS Universidad_Nombre,
+        candidato.Tiempo_Restante,
+        modalidad_trabajo.Modalidad AS Modalidad_Trabajo_Nombre,
+        candidato.CV
+    FROM candidato
+    INNER JOIN estado ON candidato.Estado = estado.ID
+    INNER JOIN sexo ON candidato.Sexo = sexo.ID
+    INNER JOIN universidad ON candidato.Universidad = universidad.ID
+    INNER JOIN modalidad_trabajo ON candidato.Modalidad_Trabajo = modalidad_trabajo.ID
+    WHERE candidato.ID = 1
+    LIMIT 1
     ";
 
     $resultado = mysqli_query($conexion, $consulta);
@@ -41,18 +41,18 @@ try {
         echo json_encode([
             'success' => true,
             'id' => $fila['ID'],
-            'nombre' => $fila['Nombre'],
+            'nombre' => $fila['Candidato_Nombre'],
             'apellido' => $fila['Apellido'],
             'fecha_nacimiento' => $fila['Fecha_Nacimiento'],
             'email' => $fila['Email'],
             'telefono' => $fila['Telefono'],
             'direccion' => $fila['Direccion'],
-            'estado' => $fila['Nombre'],
-            'sexo' => $fila['Nombre'],
+            'estado' => $fila['Estado_Nombre'],
+            'sexo' => $fila['Sexo_Nombre'],
             'fotografia' => $fila['Fotografia'],
-            'universidad' => $fila['Nombre'],
+            'universidad' => $fila['Universidad_Nombre'],
             'tiempo_restante' => $fila['Tiempo_Restante'],
-            'modalidad_trabajo' => $fila['Nombre'],
+            'modalidad_trabajo' => $fila['Modalidad_Trabajo_Nombre'],
             'cv' => $fila['CV']
         ]);
     } else {
