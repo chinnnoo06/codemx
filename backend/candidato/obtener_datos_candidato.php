3bdf1c5b38d8cc02a5efbc93973f32e9dda7e42d
chinnoo_06 <inda5054@gmail.com>
2025-01-15 02:14:52 -0600

    act

--- backend/candidato/obtener_datos_candidato.php
+++ backend/candidato/obtener_datos_candidato.php
@@ -1,18 +1,9 @@
 <?php
 require_once '../config/conexion.php';
 
-/*session_start(); // Inicia la sesión
-
-if (!isset($_SESSION['usuario'])) {
-    header('Location: https://www.codemx.net/codemx/frontend/build');
-    exit();
-}
-
-$emailUsuario = $_SESSION['usuario'];
-*/
 try {
     // Consulta para obtener los datos completos del candidato y los nombres de las tablas relacionadas
-   $consulta = "
+    $consulta = "
     SELECT 
         candidato.ID,
         candidato.Nombre AS Candidato_Nombre,
@@ -37,30 +28,6 @@ try {
     LIMIT 1
     ";
 
-   /* $consulta = "
-    SELECT 
-        candidato.ID,
-        candidato.Nombre AS Candidato_Nombre,
-        candidato.Apellido,
-        candidato.Fecha_Nacimiento,
-        candidato.Email,
-        candidato.Telefono,
-        candidato.Direccion,
-        estado.Nombre AS Estado_Nombre,
-        sexo.Sexo AS Sexo_Nombre,
-        candidato.Fotografia,
-        universidad.Nombre AS Universidad_Nombre,
-        candidato.Tiempo_Restante,
-        modalidad_trabajo.Modalidad AS Modalidad_Trabajo_Nombre,
-        candidato.CV
-    FROM candidato
-    INNER JOIN estado ON candidato.Estado = estado.ID
-    INNER JOIN sexo ON candidato.Sexo = sexo.ID
-    INNER JOIN universidad ON candidato.Universidad = universidad.ID
-    INNER JOIN modalidad_trabajo ON candidato.Modalidad_Trabajo = modalidad_trabajo.ID
-    WHERE candidato.Email = '$emailUsuario'
-    LIMIT 1";
-*/
     $resultado = mysqli_query($conexion, $consulta);
 
     if (!$resultado) {
