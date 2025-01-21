0338c0216569c6d82e2c6a3392d379eb4a8daccb
chinnoo_06 <inda5054@gmail.com>
2025-01-20 18:58:59 -0600

    act

--- frontend/src/components/bienvenida/Seccion1Bienvenida.jsx
+++ frontend/src/components/bienvenida/Seccion1Bienvenida.jsx
@@ -9,10 +9,10 @@ export const Seccion1Bienvenida = () => {
       <div className="row align-items-center">
         {/* Columna Izquierda: Información */}
         <div className="info col-md-6">
-          <h2 className="display-1 fw-bold mb-4">CODEMX</h2>
+          <h2 className="display-1 fw-bold mb-4" translate="no">CODEMX</h2>
           <p className="text-uppercase fw-semibold fs-4 mb-4">¡El inicio de tu vida profesional!</p>
           <p className="descripcion fs-5">
-            Inicia sesión o regístrate en <span className="span fw-semibold">CODEMX</span> para encontrar el trabajo de tus sueños como programador. Al iniciar sesión aceptas los términos de política de privacidad y las condiciones de uso de la plataforma de <span className="span fw-semibold">CODEMX</span>.
+            Inicia sesión o regístrate en <span className="span fw-semibold" translate="no">CODEMX</span> para encontrar el trabajo de tus sueños como programador. Al iniciar sesión aceptas los términos de política de privacidad y las condiciones de uso de la plataforma de <span className="span fw-semibold" translate="no">CODEMX</span>.
           </p>
         </div>
 
