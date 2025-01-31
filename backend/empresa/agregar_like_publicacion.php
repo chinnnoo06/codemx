f77d191709b5fde52dbfd186af8f02a73368b0cb
chinnoo_06 <inda5054@gmail.com>
2025-01-31 00:48:22 -0600

    act

---
+++ backend/empresa/agregar_like_publicacion.php
@@ -0,0 +1,42 @@
+<?php
+require_once '../config/conexion.php';
+
+// Encabezados para habilitar CORS
+$allowed_origin = 'https://www.codemx.net';
+header("Access-Control-Allow-Origin: $allowed_origin");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+// Manejo del método OPTIONS (Preflight)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204); // No Content
+    exit();
+}
+
+try {
+    // Obtén el cuerpo de la solicitud
+    $data = json_decode(file_get_contents('php://input'), true);
+    $fechaActual = date('Y-m-d H:i:s');
+
+    if (!isset($data['idCandidato'])) {
+        echo json_encode(['error' => 'Falta el ID del candidato.']);
+        http_response_code(400);
+        exit();
+    }
+
+    $idEmpresa = mysqli_real_escape_string($conexion, $data['idEmpresa']);
+    $idComentario = mysqli_real_escape_string($conexion, $data['idComentario']);
+
+    $consulta = " INSERT INTO reacciones_comentarios (Comentario_ID, Empresa_ID, Fecha_Reaccion)
+    VALUES ('$idComentario', '$idEmpresa', '$fechaActual')";
+
+    if (mysqli_query($conexion, $consulta)) {
+        echo json_encode(['success' => true, 'message' => 'Like agregado.']);
+    } else {
+        echo json_encode(['error' => false, 'error' => 'Error al agregar: ' . mysqli_error($conexion)]);
+    }
+} catch (Exception $e) {
+    echo json_encode(['success' => false, 'error' => 'Error del servidor: ' . $e->getMessage()]);
+}
+?>
