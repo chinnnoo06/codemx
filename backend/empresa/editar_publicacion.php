ca513836215e1c208fd5c924450902b4eeca5da7
chinnoo_06 <inda5054@gmail.com>
2025-02-04 13:07:26 -0600

    act

---
+++ backend/empresa/editar_publicacion.php
@@ -0,0 +1,22 @@
+<?php
+require_once '../config/conexion.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $idPublicacion = mysqli_real_escape_string($conexion, $_POST['publicacion_id']);
+    $descripcion = mysqli_real_escape_string($conexion, $_POST['descripcion']);
+    $ocultarMeGusta = mysqli_real_escape_string($conexion, $_POST['ocultar_me_gusta']);
+    $sinComentarios = mysqli_real_escape_string($conexion, $_POST['sin_comentarios']);
+  
+    $consulta = "ALTER TABLE publicacion SET Contenido = '$descripcion', Ocultar_MeGusta = '$ocultarMeGusta', Sin_Comentarios = '$sinComentarios'";
+
+    if (mysqli_query($conexion, $consulta)) {
+        echo json_encode(['success' => 'Publicacion editada corrctamente']);
+    } else {
+        die(json_encode(['error' => 'Error al guardar en la base de datos: ' . mysqli_error($conexion)]));
+    }
+
+} else {
+    http_response_code(405); 
+    echo json_encode(['error' => 'El método no está permitido.']);
+}
+?>
