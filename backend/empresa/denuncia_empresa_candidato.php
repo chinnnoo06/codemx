f8d0139d8c885d38edd1756e865147de508afee1
chinnoo_06 <inda5054@gmail.com>
2025-02-03 18:23:34 -0600

    act

---
+++ backend/empresa/denuncia_empresa_candidato.php
@@ -0,0 +1,62 @@
+<?php
+require_once '../config/conexion.php';
+
+// Configuración de CORS
+$allowed_origin = 'https://www.codemx.net';
+header("Access-Control-Allow-Origin: $allowed_origin");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+// Manejo del método OPTIONS (Preflight)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204); // No Content
+    exit();
+}
+
+try {
+    // Obtén el cuerpo de la solicitud
+    $data = json_decode(file_get_contents('php://input'), true);
+    $fechaActual = date('Y-m-d H:i:s');
+
+    if ((!isset($data['idEmpresa'])) || !isset($data['idCandidato']) || !isset($data['motivo']) || !isset($data['descripcion'])) {
+        echo json_encode(['success' => false, 'error' => 'Falta datos importantes para realizar la denuncia.']);
+        http_response_code(400);
+        exit();
+    }
+
+    $idCandidato = mysqli_real_escape_string($conexion, $data['idPublicacion']);
+    $idEmpresa = mysqli_real_escape_string($conexion, $data['comentario']);
+    $idMotivo = mysqli_real_escape_string($conexion, $data['motivo']);
+    $descripcion = mysqli_real_escape_string($conexion, $data['descripcion']);
+    $estado = mysqli_real_escape_string($conexion, $data['estado']);
+
+    $idComentario = isset($data['idComentario']) && !empty($data['idComentario']) 
+        ? "'" . mysqli_real_escape_string($conexion, $data['idComentario']) . "'" 
+        : "NULL";
+
+    $idChat = isset($data['idChat']) && !empty($data['idChat']) 
+    ? "'" . mysqli_real_escape_string($conexion, $data['idChat']) . "'" 
+    : "NULL";
+
+    $idMensaje = isset($data['idMensaje']) && !empty($data['idMensaje']) 
+    ? "'" . mysqli_real_escape_string($conexion, $data['idMensaje']) . "'" 
+    : "NULL";
+
+    // Consulta para insertar el nuevo comentario
+    $consulta = "INSERT INTO denuncia_empresa_candidato (Denunciante_ID, Denunciado_ID, Motivo, Estado_Denuncia, Descripcion, Fecha_Denuncia, Comentario_ID, Chat_ID, Mensaje-ID)
+                VALUES ('$idEmpresa', '$idCandidato', '$idMotivo', '$estado', '$descripcion', '$fechaActual', $idComentario, $idChat, $idMensaje)";
+
+    
+
+    if (mysqli_query($conexion, $consulta)) {
+        echo json_encode(['success' => true, 'message' => 'Denuncia agregada.']);
+    } else {
+        echo json_encode(['success' => false, 'error' => 'Error al agregar: ' . mysqli_error($conexion)]);
+        http_response_code(500);
+    }
+} catch (Exception $e) {
+    echo json_encode(['success' => false, 'error' => 'Error del servidor: ' . $e->getMessage()]);
+    http_response_code(500);
+}
+?>
