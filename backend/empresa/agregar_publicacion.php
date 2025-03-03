e86048f0c6d157dae68ab375248dc64b7edbfb90
chinnoo_06 <inda5054@gmail.com>
2025-03-03 16:57:00 -0600

    act

--- backend/empresa/agregar_publicacion.php
+++ backend/empresa/agregar_publicacion.php
@@ -2,6 +2,7 @@
 require_once '../config/conexion.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Recibir los datos enviados
     $idEmpresa = mysqli_real_escape_string($conexion, $_POST['empresa_id']);
     $descripcion = mysqli_real_escape_string($conexion, $_POST['descripcion']);
     $ocultarMeGusta = mysqli_real_escape_string($conexion, $_POST['ocultar_me_gusta']);
@@ -15,31 +16,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $imgDirRelativo = '/resources/publicaciones/';
     $imgDir = realpath(__DIR__ . '/../../public/resources/publicaciones/');
 
-    // Guardar img de la publicacion
+    // Verificar si la imagen fue subida correctamente
     $imgRutaCompleta = null;
     if (isset($_FILES['imagen']) && $_FILES['imagen']['error'] === UPLOAD_ERR_OK) {
-        $imgNumero = count(glob($imgDir . "/publi*")) + 1;
-        $imgNombre = "publi" . $imgNumero . "." . pathinfo($_FILES['imagen']['name'], PATHINFO_EXTENSION);
+        // Generar un identificador único para el nombre de la imagen
+        $imgNombre = "publi" . uniqid() . "." . pathinfo($_FILES['imagen']['name'], PATHINFO_EXTENSION);
         $imgRutaRelativa = $imgDirRelativo . $imgNombre;
         $imgRutaCompleta = $serverUrl . $imgRutaRelativa;
+
+        // Mover la imagen al directorio de almacenamiento
         if (!move_uploaded_file($_FILES['imagen']['tmp_name'], $imgDir . '/' . $imgNombre)) {
             die(json_encode(['error' => 'Error al guardar la imagen de la publicacion.']));
         }
     } else {
         die(json_encode(['error' => 'Falta la imagen']));
     }
-  
+
+    // Insertar los datos de la publicación en la base de datos
     $consulta = "INSERT INTO publicacion (Empresa_ID, Img, Contenido, Ocultar_MeGusta, Sin_Comentarios, Fecha_Publicacion)
         VALUES ('$idEmpresa', '$imgRutaCompleta', '$descripcion', '$ocultarMeGusta', '$sinComentarios', '$fechaActual')";
 
+    // Verificar si la consulta se ejecutó correctamente
     if (mysqli_query($conexion, $consulta)) {
-        echo json_encode(['success' => 'Publicacion subida corrctamente']);
+        echo json_encode(['success' => 'Publicacion subida correctamente']);
     } else {
         die(json_encode(['error' => 'Error al guardar en la base de datos: ' . mysqli_error($conexion)]));
     }
 
 } else {
-    http_response_code(405); 
+    // Si la solicitud no es POST, devolver un error 405
+    http_response_code(405);
     echo json_encode(['error' => 'El método no está permitido.']);
 }
 ?>
