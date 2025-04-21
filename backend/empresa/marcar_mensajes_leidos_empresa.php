5b3ae1af6c430cb3841a1f80938bca85c8fd04d9
chinnoo_06 <inda5054@gmail.com>
2025-04-21 03:45:33 -0600

    act

---
+++ backend/empresa/marcar_mensajes_leidos_empresa.php
@@ -0,0 +1,32 @@
+<?php
+require_once '../config/conexion.php';
+
+// Encabezados para habilitar CORS
+$allowed_origin = 'https://www.codemx.net';
+header("Access-Control-Allow-Origin: $allowed_origin");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+// Manejo del método OPTIONS (Preflight)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204); // No Content
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents("php://input"), true);
+    $chatID = mysqli_real_escape_string($conexion, $data['chatID']);
+
+    $query = "UPDATE mensajes SET Lectura = 1 WHERE Chat_ID = '$chatID' AND Usuario = 'candidato' AND Lectura = 0";
+
+    if (mysqli_query($conexion, $query)) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['success' => false, 'error' => mysqli_error($conexion)]);
+    }
+} else {
+    http_response_code(405); 
+    echo json_encode(['error' => 'El método no está permitido.']);
+}
+?>
