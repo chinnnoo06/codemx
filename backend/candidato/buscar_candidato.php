8454ac80f443f5ab6f4e5a77763c514e85116ac6
chinnoo_06 <inda5054@gmail.com>
2025-04-29 02:03:51 -0600

    act

--- backend/candidato/buscar_candidato.php
+++ backend/candidato/buscar_candidato.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $query = mysqli_real_escape_string($conexion, $data['query']);
     $idCandidato = mysqli_real_escape_string($conexion, $data['idCandidato']); // ID del candidato para excluir
     $page = (int)$data['page'];
-    $limit = 5;  // Número de elementos a devolver por página
+    $limit = 15;  // Número de vacantes a devolver por página
     $offset = ($page - 1) * $limit; // Calcular el offset según la página
 
     // Consultar en la tabla de candidatos, excluyendo el perfil del usuario que realiza la búsqueda
@@ -66,14 +66,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 
-    // Paginación: Limitar el número de resultados devueltos
-    $paginatedResults = array_slice($usuarios, $offset, $limit);
-
-    // Devolver los resultados en formato JSON como un solo arreglo paginado
-    echo json_encode($paginatedResults);
+    // Devolver los resultados en formato JSON como un solo arreglo
+    echo json_encode($usuarios);
 
 } else {
     http_response_code(405); 
     echo json_encode(['error' => 'El método no está permitido.']);
 }
 ?>
+
+
