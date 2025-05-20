d13448f5943329b417df3b8b2b5d4aa9b2030e54
chinnnoo06 <inda5054@gmail.com>
2025-05-19 23:22:54 -0600

    act

--- backend/admin/obtener_denuncias.php
+++ backend/admin/obtener_denuncias.php
@@ -34,7 +34,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         candidato_denunciado.Nombre AS Nombre_Denunciado,
         candidato_denunciado.Apellido AS Apellido_Denunciado,
         candidato_denunciado.Fotografia AS Foto_Denunciado,
-        'Candidato a Candidato' AS Tipo_Denuncia
+        'Candidato a Candidato' AS Tipo_Denuncia,
+        NULL AS Mensaje,
+        NULL AS Fecha_Envio,
+        NULL AS Publicacion_ID,
+        NULL AS Vacante_ID
     FROM denuncia_candidato_candidato
     INNER JOIN motivos_denuncia_candidato ON denuncia_candidato_candidato.Motivo = motivos_denuncia_candidato.ID
     INNER JOIN estado_denuncia ON denuncia_candidato_candidato.Estado_Denuncia = estado_denuncia.ID
@@ -55,17 +59,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         denuncia_candidato_empresa.Comentario_ID,
         comentarios.Comentario,
         comentarios.Fecha_Comentario,
-        denuncia_candidato_empresa.Mensaje_ID,
-        mensajes.Mensaje,
-        mensajes.Fecha_Envio,
-        denuncia_candidato_empresa.Publicacion_ID,
-        denuncia_candidato_empresa.Vacante_ID,
         candidato_denunciante.Nombre AS Nombre_Denunciante,
         candidato_denunciante.Apellido AS Apellido_Denunciante,
         candidato_denunciante.Fotografia AS Foto_Denunciante,
         empresa_denunciado.Nombre AS Nombre_Denunciado,
         empresa_denunciado.Logo AS Foto_Denunciado,
-        'Candidato a Empresa' AS Tipo_Denuncia
+        'Candidato a Empresa' AS Tipo_Denuncia,
+        mensajes.Mensaje,
+        mensajes.Fecha_Envio,
+        denuncia_candidato_empresa.Publicacion_ID,
+        denuncia_candidato_empresa.Vacante_ID
     FROM denuncia_candidato_empresa
     INNER JOIN motivos_denuncia_empresa ON denuncia_candidato_empresa.Motivo = motivos_denuncia_empresa.ID
     INNER JOIN estado_denuncia ON denuncia_candidato_empresa.Estado_Denuncia = estado_denuncia.ID
@@ -87,15 +90,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         denuncia_empresa_candidato.Comentario_ID,
         comentarios.Comentario,
         comentarios.Fecha_Comentario,
-        denuncia_empresa_candidato.Mensaje_ID,
-        mensajes.Mensaje,
-        mensajes.Fecha_Envio,
         empresa_denunciante.Nombre AS Nombre_Denunciante,
         empresa_denunciante.Logo AS Foto_Denunciante,
         candidato_denunciado.Nombre AS Nombre_Denunciado,
         candidato_denunciado.Apellido AS Apellido_Denunciado,
         candidato_denunciado.Fotografia AS Foto_Denunciado,
-        'Empresa a Candidato' AS Tipo_Denuncia
+        'Empresa a Candidato' AS Tipo_Denuncia,
+        mensajes.Mensaje,
+        mensajes.Fecha_Envio,
+        NULL AS Publicacion_ID,
+        NULL AS Vacante_ID
     FROM denuncia_empresa_candidato
     INNER JOIN motivos_denuncia_candidato ON denuncia_empresa_candidato.Motivo = motivos_denuncia_candidato.ID
     INNER JOIN estado_denuncia ON denuncia_empresa_candidato.Estado_Denuncia = estado_denuncia.ID
